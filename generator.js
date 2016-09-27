6a007d654e03e2f04dfb7f57e72f764bb6ac1f0c
gejiawen <806717031@qq.com>
2016-09-27 16:00:55 +0800

    update for test

---
+++ generator.js
@@ -0,0 +1,31 @@
+/**
+ * @file: generator
+ * @author: gejiawen
+ * @date: 9/27/16 15:59
+ * @description: generator
+ */
+/**
+ * @file: index.js
+ * @author: gejiawen
+ * @date: 9/27/16 15:40
+ * @description: index.js
+ */
+
+var isValid = require('is-valid-app');
+
+module.exports = function (app) {
+    if (!isValid(app, 'generate-npmrc')) {
+        return
+    }
+
+    app.task('default', ['npmrc'])
+
+    app.task('npmrc', function (cb) {
+        return app.src('templates/.npmrc', {cwd: __dirname})
+            .pipe(app.conflicts(app.cwd))
+            .pipe(app.dest(function (file) {
+                file.basename = '.npmrc'
+                return app.cwd
+            }))
+    })
+}
--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-/**
- * @file: index.js
- * @author: gejiawen
- * @date: 9/27/16 15:40
- * @description: index.js
- */
+module.exports = require('./generator')
